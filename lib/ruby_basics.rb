2d53e54a90f676f1339ee84dac0b81a321c84651
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-18 10:19:30 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+ return (num1)/(num2)
 end
+division(12,2)
 
 def assign_variable(value)
-  
+  return value
+end
+assign_variable("Nice")
+
+def argue(argumentAccepted)
+  return argumentAccepted
 end
 
-def argue
-  
+def greeting(arg1,arg2)
+  return "It is nice to see " + arg1 + " and " + arg2 + "."
 end
 
-def greeting
-
+def return_a_value(lowercaseDummy = "Nice")
+  return lowercaseDummy
 end
 
-def return_a_value
-
+def last_evaluated_value(arg1 = "expert")
+   arg1
 end
 
-def last_evaluated_value
-
-end
-
-def pizza_party
-
+def pizza_party(vegan="cheese")
+  return vegan
 end
